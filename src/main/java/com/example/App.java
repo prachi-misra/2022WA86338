ef837e0bfdb83c1618149e8ea91289bb5dc22c78
prachi-misra <prachi.misra92@gmail.com>
2026-09-24 22:28:29 +0530

    Updated App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,5 +4,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello from BITS DevOps Lab!");
+        System.out.println("My BITS ID is 2022WA86338!");
     }
 }
